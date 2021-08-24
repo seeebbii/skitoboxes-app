04029aa59392ab72404494f2b290a387873d5600
haseeb zafar <Haseebzafar107@gmail.com>
2021-08-24 22:47:26 +0500

    added few files

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const orange = Color(0xFFFB8500);
+const yellow = Color(0xFFFFB703);
+const darkBlue = Color(0xFF023047);
+const blue = Color(0xFF219EBC);
+const lightBlue = Color(0xFF8ECAE6);
--- lib/views/splash/main_splash_screen.dart
+++ lib/views/splash/main_splash_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:skitoboxes/constants/colors.dart';
 class MainSplashScreen extends StatelessWidget {
   const MainSplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: orange,
       body: Center(
         child: Text('SPLASH', style: TextStyle(fontSize: 20) ,),
       ),
