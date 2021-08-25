3e728ea42ff13bf1c4663d5c4ea50d1ed08d751b
Ahmed Navaz <ahmednawaz.dev@gmail.com>
2021-08-26 02:53:01 +0500

    ni

--- lib/views/splash/main_splash_screen.dart
+++ lib/views/splash/main_splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skitoboxes/constants/colors.dart';
 import 'package:rive/rive.dart';
+import 'package:skitoboxes/views/splash/screen_title.dart';
 
 class MainSplashScreen extends StatelessWidget {
   const MainSplashScreen({Key? key}) : super(key: key);
@@ -21,29 +22,7 @@ class MainSplashScreen extends StatelessWidget {
               ),
             ),
             Container(
-              child: TweenAnimationBuilder(
-                duration: Duration(milliseconds: 1000),
-                builder: (BuildContext context, double _val, child) {
-                  return Opacity(
-                    opacity: _val,
-                    child: Padding(
-                      padding: EdgeInsets.only(bottom: _val * 50),
-                      child: child,
-                    ),
-                  );
-                },
-                tween: Tween<double>(begin: 0, end: 1),
-                curve: Curves.easeIn,
-                child: Text(
-                  'SKITO',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Akira',
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+              child: ScreenTitle(text: 'SKITO'),
             ),
           ],
         ),
---
+++ lib/views/splash/screen_title.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ScreenTitle extends StatelessWidget {
+  final String? text;
+
+  ScreenTitle({Key? key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      duration: Duration(milliseconds: 1000),
+      builder: (BuildContext context, double _val, child) {
+        return Opacity(
+          opacity: _val,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: _val * 50),
+            child: child,
+          ),
+        );
+      },
+      tween: Tween<double>(begin: 0, end: 1),
+      curve: Curves.easeIn,
+      child: Text(
+        text!,
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'Akira',
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
